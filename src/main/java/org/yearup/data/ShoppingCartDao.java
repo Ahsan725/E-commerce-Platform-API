1d61b88a2b9ce7d476f38aceae6f544cde9aeda7
Ahsan <sbaseer25@gmail.com>
2025-12-16 10:08:16 -0500

    feature: modified the ShoppingCartDao interface to add methods

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -2,8 +2,17 @@ package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
 
-public interface ShoppingCartDao
-{
+public interface ShoppingCartDao {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+
+    void addProduct(int userId, int productId);
+
+    void updateQuantity(int userId, int productId, int quantity);
+
+    boolean exists(int userId, int productId);
+
+    int getQuantity(int userId, int productId);
+
+    void clearCart(int userId);
 }
