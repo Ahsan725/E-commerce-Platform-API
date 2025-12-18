e5316f92dd00faba151c366fd3eb57c135c52a55
Ahsan <sbaseer25@gmail.com>
2025-12-18 08:54:09 -0500

    updated the profile dao

--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -1,9 +1,12 @@
 package org.yearup.data;
 
-
 import org.yearup.models.Profile;
 
 public interface ProfileDao
 {
     Profile create(Profile profile);
+
+    Profile getByUserId(int userId); //adeed  a new method
+
+    void update(Profile profile); // added a mew method
 }
