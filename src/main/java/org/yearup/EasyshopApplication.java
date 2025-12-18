2b8401cefddb5a579b566f39148f16fc7902ea41
Ahsan <sbaseer25@gmail.com>
2025-12-18 17:34:15 -0500

    added comments

--- src/main/java/org/yearup/EasyshopApplication.java
+++ src/main/java/org/yearup/EasyshopApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EasyshopApplication
 {
+// this is the entry point into the backend api
 
     public static void main(String[] args) {
         SpringApplication.run(EasyshopApplication.class, args);
