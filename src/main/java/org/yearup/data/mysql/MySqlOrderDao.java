a9662e5b6283597700abcf093d4994ed4b12c1da
Ahsan <sbaseer25@gmail.com>
2025-12-18 14:08:55 -0500

    created the My Sql Order Dao

---
+++ src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
@@ -0,0 +1,42 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.OrderDao;
+import org.yearup.models.Order;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+
+@Component
+public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
+    public MySqlOrderDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public Order create(Order order) {
+        String sql = """
+                INSERT INTO orders (user_id, order_date, status)
+                VALUES (?, ?, ?)
+                """;
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, order.getUserId());
+            statement.setTimestamp(2, Timestamp.valueOf(order.getOrderDate()));
+            statement.setString(3, order.getStatus());
+
+            statement.executeUpdate();
+
+            ResultSet keys = statement.getGeneratedKeys();
+            if (keys.next()) {
+                order.setOrderId(keys.getInt(1));
+            }
+
+            return order;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
